7f489f9296af40d5a19f1a33841d7c7fe0f9a936
varun <varun8.a@tcs.com>
2019-10-14 10:19:29 +0200

    ok

---
+++ ParallelTestScript/MessageClient.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelTestScript
+{
+   public  class MessageClient
+    {
+        private HttpClient client;
+
+        public MessageClient()
+        {
+            client = new HttpClient();
+        }
+
+        private static async Task<bool> SendToEconnect(string message, TextWriter log)
+        {
+           
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://mobileapp-meap-qa.ase-meap-onprem-prodqa.p.azurewebsites.net/tables/MessageQueue");
+
+                    //var authData = string.Format("{0}:{1}", MeapUserName, MeapUserPassword);
+                    //var authHeaderValue = Convert.ToBase64String(Encoding.UTF8.GetBytes(authData));
+                    //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authHeaderValue);
+                    //client.DefaultRequestHeaders.Accept.Clear();
+
+                    //message = message.Replace("Header2", "Header");
+                    HttpContent stringContent = new StringContent(message, UnicodeEncoding.UTF8, "application/json");
+                    client.DefaultRequestHeaders.Add("ZUMO-API-VERSION", "2.0.0");
+                    //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue
+
+                    HttpResponseMessage response = await client.PostAsync("", stringContent);
+
+                    log.WriteLine("GOT reponse from econnect");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string data = await response.Content.ReadAsStringAsync();
+                        log.WriteLine(data);
+                    }
+                    else
+                    {
+                        log.WriteLine(response.ToString());
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+                // Need to throw other wise message would be removed from service bus.
+                throw;
+            }
+            return true;
+        }
+
+        public async Task<IEnumerable<bool>> GetUsersInParallel(IEnumerable<string> msgs)
+        {
+            TextWriter tx =new StringWriter();
+            var tasks = msgs.Select(msg => SendToEconnect(msg,tx));
+            var users = await Task.WhenAll(tasks);
+
+            return users;
+        }
+    }
+}
---
+++ ParallelTestScript/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ParallelTestScript
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(DateTime.Now.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fff"));
+            //Console.WriteLine(DateTime.Now.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.ffffff"));
+            //Console.WriteLine(DateTime.Now.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffzzzz"));
+            //Console.WriteLine(DateTime.Now.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ss.fff+00:00"));
+            //Console.WriteLine(DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.fff"));
+            //Console.WriteLine(DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.ffffffzzzz"));
+            //Console.WriteLine(DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.fffzzzz"));
+            //TimeZone localZone = TimeZone.CurrentTimeZone;
+            //TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("Central Europe Standard Time")).ToString("yyyy-MM-ddTHH:mm:ss.fffffffzzzz");
+            //string str = System.IO.File.ReadAllText(@"D:\\ParallelTestScript\\ParallelTestScript\\Request.txt");
+            Task.Run(async () => await Measure());
+            System.Console.ReadKey();
+        }
+
+        private static async Task Measure()
+        {
+            var services = new MessageClient();
+            var stopper = new Stopwatch();
+            var msgs = GetRequests(10);
+
+            
+
+            stopper.Start();
+            await services.GetUsersInParallel(msgs);
+            System.Console.WriteLine("In paralell: " + stopper.Elapsed);
+
+           
+        }
+
+        private static IEnumerable<string> GetRequests(int numberOfIds)
+        {
+            int num = 24000;
+            for (int i = 1; i <= numberOfIds; i++)
+            {
+                string str= System.IO.File.ReadAllText(@"D:\\ParallelTestScript\\ParallelTestScript\\RequestDamage.txt");
+                str = str.Replace("50000", num++.ToString());
+                str = str.Replace("01698914-9cca-42c1-9822-0fcd1a5c7054", Guid.NewGuid().ToString());
+                yield return str  ;
+            }
+        }
+    }
+}
